afb38c05eae696315e01ab8dc1a7b1a3109cc008
Bassam Husain <bassam.husain@gmail.com>
2023-12-21 06:17:19 -0500

    First commit.

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let a = fizz_buzz(100);
+    println!("{:?}", a)
+}
+
+pub fn fizz_buzz(n: i32) -> Vec<String> {
+    let mut ans: Vec<String> = Vec::new();
+
+    for number in 1..(n + 1) {
+        let a = {
+            if number % 3 == 0 && number % 5 == 0 {
+                "FizzBuzz".to_string()
+            } else if number % 3 == 0 {
+                "Fizz".to_string()
+            } else if number % 5 == 0 {
+                "Buzz".to_string()
+            } else {
+                number.to_string()
+            }
+        };
+        ans.push(a.to_string());
+    }
+    ans
+}
